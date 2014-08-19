1e76054abf87d93bd934253aeebf305cc9677635
Kris C <kris541@hotmail.com>
2014-08-19 14:22:18 -0500

    added 2 functions - list_items & get_input

--- todo.php
+++ todo.php
@@ -1,44 +1,63 @@
 <?php
 
-// Create array to hold list of todo items
-$items = array();
+ // Create array to hold list of todo items
+ $items = array();
 
-// The loop!
-do {
-    // Iterate through list items
-    foreach ($items as $key => $item) {
-        // Display each item and a newline
-        echo ($key + 1) . " {$item}\n";
+ // List array items formatted for CLI
+ function list_items($list)
+ {
+    $new_list = ' ';
+    foreach ($list as $key => $item) {
+        $new_list .= ($key + 1) . " {$item}\n";
     }
+        return $new_list;
 
-    // Show the menu options
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
+ }
 
-    // Get the input from user
-    // Use trim() to remove whitespace and newlines
-    $input = strtoupper(trim(fgets(STDIN)));
-
-
-    // Check for actionable input
-    if ($input == 'N') {
-        // Ask for entry
-        echo 'Enter item: ';
-        // Add entry to list array
-        $items[] = trim(fgets(STDIN));
-    } elseif ($input == 'R') {
-        // Remove which item?
-        echo 'Enter item number to remove: ';
-        // Get array key
-        $key = trim(fgets(STDIN));
-        $key = $key - 1;
-        // Remove from array
-        unset($items[$key]);
+ // Get STDIN, strip whitespace and newlines, 
+ // and convert to uppercase if $upper is true
+ function get_input($upper = FALSE) 
+ {
+     if ($upper) {
+        return  strtoupper(trim(fgets(STDIN)));       
+    } else {
+        return trim(fgets(STDIN));
     }
-// Exit when input is (Q)uit
-} while ($input != 'Q');
+}
 
-// Say Goodbye!
-echo "Goodbye!\n";
+ // The loop!
+ do {
+     // Echo the list produced by the function
+     echo list_items($items);
 
-// Exit with 0 errors
-exit(0);
+     // Show the menu options
+     echo '(N)ew item, (R)emove item, (Q)uit : ';
+
+     // Get the input from user
+     // Use trim() to remove whitespace and newlines
+     $input = get_input(TRUE);
+
+     // Check for actionable input
+     if ($input == 'N') {
+         // Ask for entry
+         echo 'Enter item: ';
+         // Add entry to list array
+         $items[] = get_input();
+     } elseif ($input == 'R') {
+         // Remove which item?
+         echo 'Enter item number to remove: ';
+         // Get array key
+         $key = get_input();
+         $key = $key - 1;
+         // Remove from array
+         unset($items[$key]);
+         $items = array_values($items);
+     }
+ // Exit when input is (Q)uit
+ } while ($input != 'Q');
+
+ // Say Goodbye!
+ echo "Goodbye!\n";
+
+ // Exit with 0 errors
+ exit(0);
